9dd94a487f99c0d32b1feb226dff35b35a08f048
kazutokato <e.t.s.w.a.n.a@gmail.com>
2020-12-05 10:48:38 +0000

    [Add] バリデーション機能

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   resources :books
-  resources :users, only: [:show, :edit]
+  resources :users, only: [:show, :edit, :update]
 end
