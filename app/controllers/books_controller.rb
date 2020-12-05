2304a9e92f6c709ef67ae136623af0fc3c7110c7
kazutokato <e.t.s.w.a.n.a@gmail.com>
2020-12-05 08:36:46 +0000

    [Start] 12/05

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,14 +1,11 @@
 class BooksController < ApplicationController
   def index
+    @book = Book.new
     @books = Book.all
   end
 
   def show
     @book = Book.find(params[:id])
-  end
-
-  def new
-    @book = Book.new
   end
 
   def create
