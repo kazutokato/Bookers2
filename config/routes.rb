0c1c6e5fe1f671b8fea5255bc9fbc0d8b915587d
kazutokato <e.t.s.w.a.n.a@gmail.com>
2020-12-08 14:46:33 +0000

    [Update] User info

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   get 'home/about'=> 'homes#about'
-  resources :home, only: [:show]
+  resources :homes, only: [:top,:about]
   resources :books
   resources :users, only: [:index, :show, :edit, :update]
 end
