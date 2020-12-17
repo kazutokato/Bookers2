d0e765de256e58e91a821bdfecb4cad1d6fff72d
kazutokato <e.t.s.w.a.n.a@gmail.com>
2020-12-17 12:38:05 +0000

    [Update] いいね機能の追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get 'home/about'=> 'homes#about'
   resources :homes, only: [:top,:about]
-  resources :books
+  resources :books do
+    resource :favorites, only: [:create, :destroy]
+    resource :book_comments, only: [:create, :destroy]
+  end
   resources :users, only: [:index, :show, :edit, :update]
 end
