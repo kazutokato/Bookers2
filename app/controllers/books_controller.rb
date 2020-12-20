e60fd6aaad969a3a6dcd96ba841ee6600e9f7fe1
kazutokato <e.t.s.w.a.n.a@gmail.com>
2020-12-20 09:11:04 +0000

    [Update] 新規投稿フォームのエラー改善

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -19,6 +19,8 @@ class BooksController < ApplicationController
   def create
     # binding.pry
     @book_new = Book.new(book_params)
+    @books = Book.all
+    @user = User.find(current_user.id)
     @book_new.user_id = current_user.id
     if @book_new.save
       flash[:notice] = "You have created book successfully."
